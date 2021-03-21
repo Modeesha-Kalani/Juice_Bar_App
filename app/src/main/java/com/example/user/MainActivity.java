b1d60ca19e60973e4199eacc3c6ab027e9f7a3e4
Kithmini97 <nimashak97@gmail.com>
2021-03-21 12:04:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_baseline_menu_24);
+
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
