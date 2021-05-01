43276f8dd440215ec3df2513484a477d30635622
Janani96-sam <janu411denipitiya@gmail.com>
2021-05-01 21:26:00 +0530

    Delete Order Activity Create

---
+++ app/src/main/java/com/example/sliit/DeleteOrder.java
@@ -0,0 +1,14 @@
+package com.example.sliit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_order);
+    }
+}
