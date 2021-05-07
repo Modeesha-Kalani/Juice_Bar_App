c4fc09b229f7c891c8cf9444884a80a1dfe6f005
Janani96-sam <janu411denipitiya@gmail.com>
2021-05-07 19:46:20 +0530

    Order Juice Update

---
+++ app/src/main/java/com/example/fruzorest/UpdateOrderFragment.java
@@ -0,0 +1,205 @@
+package com.example.fruzorest;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.example.fruzorest.model.Order;
+import com.example.fruzorest.model.Util;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Date;
+
+
+public class UpdateOrderFragment extends Fragment {
+
+    private Bundle bundle;
+    private TextView total, pname, rprice, lprice, rqty, lqty;
+    private Button rinbtn, rdebtn, linbtn, ldebtn, updateorderbtn;
+    private TimePicker timepicker;
+    private double rpriceof, lpriceof, tot;
+    private int rqtyof, larqtyof;
+    private View.OnClickListener rinlistner;
+    private View.OnClickListener rdelistner;
+    private View.OnClickListener linlistner;
+    private View.OnClickListener ldelistner;
+    private View.OnClickListener updatelistner;
+    private Order o;
+
+
+    public UpdateOrderFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        bundle = getArguments();
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update_order, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        InitApp();
+
+    }
+
+    private void InitApp() {
+        total = getView().findViewById(R.id.pu_tot);
+        pname = getView().findViewById(R.id.pu_pname);
+        rprice = getView().findViewById(R.id.pu_regprice);
+        lprice = getView().findViewById(R.id.pu_largeprice);
+        rqty = getView().findViewById(R.id.puinteger_number);
+        lqty = getView().findViewById(R.id.puinteger_number2);
+        timepicker = getView().findViewById(R.id.timePicker2);
+        rinbtn = getView().findViewById(R.id.puincrease);
+        rdebtn = getView().findViewById(R.id.pudecrease);
+        linbtn = getView().findViewById(R.id.puincrease2);
+        ldebtn = getView().findViewById(R.id.pudecrease2);
+        updateorderbtn = getView().findViewById(R.id.updateorderbtn);
+        //set Listners to btns
+        rinlistner = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rqtyof++;
+                rqty.setText(rqtyof + "");
+                updateTot();
+            }
+        };
+        rinbtn.setOnClickListener(rinlistner);
+
+        rdelistner = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (rqtyof != 0) {
+                    rqtyof--;
+                }
+                rqty.setText(rqtyof + "");
+                updateTot();
+            }
+        };
+        rdebtn.setOnClickListener(rdelistner);
+
+        linlistner = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                larqtyof++;
+                lqty.setText(larqtyof + "");
+                updateTot();
+            }
+        };
+        linbtn.setOnClickListener(linlistner);
+
+        ldelistner = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (larqtyof != 0) {
+                    larqtyof--;
+                }
+                lqty.setText(larqtyof + "");
+                updateTot();
+            }
+        };
+        ldebtn.setOnClickListener(ldelistner);
+
+        String orderidof = bundle.getString("orderid");
+        DatabaseReference child = FirebaseDatabase.getInstance().getReference("order").child("allorder").child(orderidof);
+        child.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Order value = snapshot.getValue(Order.class);
+                o = value;
+                pname.setText(value.getPname());
+                rqty.setText("" + value.getRqty());
+                lqty.setText("" + value.getLqty());
+                total.setText(value.getTotal() + "");
+                rprice.setText(value.getRprice() + "");
+                lprice.setText(value.getLprice() + "");
+                timepicker.setHour(value.getHour());
+                timepicker.setMinute(value.getMinuts());
+                rpriceof = value.getRprice();
+                rqtyof = value.getRqty();
+                lpriceof = value.getLprice();
+                larqtyof = value.getLqty();
+                tot = value.getTotal();
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        updatelistner = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                o.setRqty(rqtyof);
+                o.setLqty(larqtyof);
+                o.setTotal(tot);
+                o.setRprice(rpriceof);
+                o.setLprice(lpriceof);
+                int hour = timepicker.getHour();
+                int minute = timepicker.getMinute();
+                o.setHour(hour);
+                o.setMinuts(minute);
+
+                DatabaseReference order = FirebaseDatabase.getInstance().getReference("order");
+                DatabaseReference allorder = order.child("allorder").child(o.getId());
+
+                Task<Void> voidTask = allorder.setValue(o);
+                voidTask.addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        DatabaseReference userorder = order.child("userorder").child(o.getUserid()).child(o.getId());
+                        Task<Void> voidTask1 = userorder.setValue(o);
+                        voidTask1.addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(getContext(), "Order Updated", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+
+
+            }
+        };
+        updateorderbtn.setOnClickListener(updatelistner);
+
+    }
+
+    private void updateTot() {
+        double rtot = rpriceof * rqtyof;
+        double ltot = lpriceof * larqtyof;
+        tot = rtot + ltot;
+        total.setText(tot + "");
+    }
+}
---
+++ app/src/main/java/com/example/fruzorest/adapter/OrderCAdapter.java
@@ -0,0 +1,65 @@
+package com.example.fruzorest.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.fruzorest.CustomerOrderViewActivity;
+import com.example.fruzorest.R;
+import com.example.fruzorest.model.Order;
+import com.example.fruzorest.viewholder.OrderViewHolder;
+import com.example.fruzorest.viewholder.ProductViewHolder;
+
+import java.util.ArrayList;
+
+public class OrderCAdapter extends RecyclerView.Adapter implements View.OnClickListener {
+    ArrayList<Order>  list;
+    Context context;
+
+    public OrderCAdapter(ArrayList<Order> list) {
+        this.list = list;
+    }
+
+    @Override
+    public void onClick(View v) {
+        LinearLayout root = v.findViewById(R.id.orderlinearlayout);
+        Order order = list.get((int) root.getTag());
+        Intent i = new Intent(context, CustomerOrderViewActivity.class);
+        i.putExtra("orderid",order.getId());
+        context.startActivity(i);
+
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.orderrecycler, parent, false);
+        inflate.setOnClickListener(this);
+        return new OrderViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        OrderViewHolder viewHolder = (OrderViewHolder) holder;
+        viewHolder.setContext(context);
+        Order order = list.get(position);
+        viewHolder.setTag(position);
+        viewHolder.setTotal(order.getTotal()+"");
+        viewHolder.setRegdetail(order.getRprice()+ " X " +order.getRqty());
+        viewHolder.setLargdetail(order.getLprice()+ " X " +order.getLqty());
+        viewHolder.setpName(order.getPname());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
