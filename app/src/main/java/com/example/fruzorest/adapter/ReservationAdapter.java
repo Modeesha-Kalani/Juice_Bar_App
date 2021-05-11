ef40915fdca5dd619877b7043524642eb8e9130d
Modeesha Kalani <65617018+Modeesha-Kalani@users.noreply.github.com>
2021-05-11 09:34:31 +0530

    adapterupdate

--- app/src/main/java/com/example/fruzorest/adapter/ReservationAdapter.java
+++ app/src/main/java/com/example/fruzorest/adapter/ReservationAdapter.java
@@ -22,46 +22,62 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class ReservationAdapter extends RecyclerView.Adapter implements View.OnClickListener {
+    //create other methods except onclick
+
     ArrayList<Reservation> list;
     Context context;
-    SimpleDateFormat format;
 
+    //get data to an arraylist
     public ReservationAdapter(ArrayList<Reservation> rlist) {
         this.list = rlist;
-        this.format = new SimpleDateFormat("yyy-MM-dd");
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //get context object
         context = parent.getContext();
+        //get layout file
+        //get that returned file
         View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.reservationrecycler, parent, false);
+
         inflate.setOnClickListener(this);
+
+        //pass view to the viewHolder class
         return new ReservationViewHolder(inflate);
     }
 
-    @Override
+    //set data into ViewHolder
+    @Override                                                               //loop
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         ReservationViewHolder viewHolder = (ReservationViewHolder) holder;
+        //pass position
         Reservation reservation = list.get(position);
         viewHolder.setTag(position);
         viewHolder.setDate(reservation.getDate());
-        viewHolder.setTime("you have Reservation From " + reservation.getTime() + " to " + reservation.getDuration());
+        viewHolder.setTime("You have a Reservation From " + reservation.getTime() + " to " + reservation.getDuration());
         viewHolder.setDuration("("+GFG.findDifference(reservation.getTime(), reservation.getDuration())+"Hours )");
+        //duration=leaving time
     }
 
     @Override
+    //get number of positions/reservations
     public int getItemCount() {
         return list.size();
     }
 
+    //when one item is clicked from all
     @Override
     public void onClick(View v) {
+        //get linear layout inside main linear layout
         LinearLayout lay = v.findViewById(R.id.re_root);
         int tag = (int) lay.getTag();
         Intent i = new Intent(context, ReservationManageActivity.class);
+        //get arraylist's one by one
         i.putExtra("reservid",list.get(tag).getId());
         context.startActivity(i);
-        
+        //pass activity as context because this is an adapter
+
     }
 }
+
